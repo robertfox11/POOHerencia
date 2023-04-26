3aafe9949a2c694ccaf74cd46d23ad00cf3a5042
Robertfox11 <11rsahome@gmail.com>
2023-04-26 13:52:43 +0200

    Herencia y el Api Reflexion JAva

--- POOHerencia/src/org/robert/ejemplo/EjemploHerencia.java
+++ POOHerencia/src/org/robert/ejemplo/EjemploHerencia.java
@@ -1,19 +1,30 @@
 package org.robert.ejemplo;
 
 import org.robert.pooherencia.Alumno;
+import org.robert.pooherencia.AlumnoInternacional;
 import org.robert.pooherencia.Persona;
 import org.robert.pooherencia.Profesor;
 
 public class EjemploHerencia {
     public static void main(String[] args) {
-        Persona alumno = new Alumno();
+        Alumno alumno = new Alumno();
         /*modificamos ejn el acceso con set */
         alumno.setNombre("Rob");
         alumno.setApellido("Fox");
+        alumno.setNotasHistoria(6.5);
+        alumno.setNotasLengua(9.8);
+        alumno.setNotasMates(8.9);
         //casteamos la instancia //modificadores de tipo de clase
-
         ((Alumno)alumno).setInstituto("Insti Nacional");
 
+        /*Alumno internacional*/
+        AlumnoInternacional alumnoInter = new AlumnoInternacional();
+        alumnoInter.setNombre("RobExtra");
+        alumnoInter.setApellido("FoxExtra");
+        alumnoInter.setPais("Canada");
+        alumnoInter.setNotasHistoria(8.5);
+        alumnoInter.setNotasLengua(7.8);
+        alumnoInter.setNotasMates(9.2);
 
         Profesor profesor = new Profesor();
         profesor.setNombre("RobertName");
@@ -23,5 +34,17 @@ public class EjemploHerencia {
         System.out.println(alumno.getNombre());
         //Casteamos la instancia
         System.out.println("Instituto del alumno " + ((Alumno)alumno).getInstituto());
+        System.out.println("***********Alumno internacional");
+        System.out.println(alumnoInter.getNombre() + " " + alumnoInter.getApellido()+ " del pais " + alumnoInter.getPais());
+        Class clase = alumno.getClass();
+        /*Clase padre sea distinto de null*/
+        while (clase.getSuperclass() != null){
+            String hija = clase.getName();
+            String padre = clase.getSuperclass().getName();
+            System.out.println(hija + " es una clase hija de la clase padre "+ padre);
+            //asignar en la clase padre
+            clase = clase.getSuperclass();
+        }
+
     }
 }
---
+++ POOHerencia/src/org/robert/pooherencia/AlumnoInternacional.java
@@ -0,0 +1,23 @@
+package org.robert.pooherencia;
+
+/**Extiende de alumno que hereda de persona */
+public class AlumnoInternacional extends Alumno{
+    private String pais;
+    private double notaIdiomas;
+
+    public String getPais() {
+        return pais;
+    }
+
+    public void setPais(String pais) {
+        this.pais = pais;
+    }
+
+    public double getNotaIdiomas() {
+        return notaIdiomas;
+    }
+
+    public void setNotaIdiomas(double notaIdiomas) {
+        this.notaIdiomas = notaIdiomas;
+    }
+}
