240bb3ee2eadc890cfdfbc7d0d1cd25e8e8fe4b0
Robertfox11 <11rsahome@gmail.com>
2023-04-26 17:08:24 +0200

    Sobre escritura de metodos en la herencia

---
+++ POOHerencia/src/org/robert/ejemplo/EjemploHerenciaConstructores.java
@@ -0,0 +1,54 @@
+package org.robert.ejemplo;
+
+import org.robert.pooherencia.Alumno;
+import org.robert.pooherencia.AlumnoInternacional;
+import org.robert.pooherencia.Persona;
+import org.robert.pooherencia.Profesor;
+
+public class EjemploHerenciaConstructores {
+    public static void main(String[] args) {
+        System.out.println("==========Creando la instancia");
+        Alumno alumno = new Alumno("Robert", "rex", 10, "Instituto Nacional");
+        alumno.setNotasHistoria(6.5);
+        alumno.setNotasLengua(9.8);
+        alumno.setNotasMates(8.9);
+        alumno.setEmail("pruebaalumna@gmail.es");
+        imprimir(alumno);
+        System.out.println("Creando la instalna de la clase alumno internacional");
+        AlumnoInternacional alumnoInter = new AlumnoInternacional("peter", "Gosling", "Belgica");
+        alumnoInter.setEdad(22);
+        alumnoInter.setInstituto("Instituto nacional");
+        alumnoInter.setNotasHistoria(8.5);
+        alumnoInter.setNotasLengua(7.8);
+        alumnoInter.setNotasMates(9.2);
+        alumnoInter.setEmail("pruebaalumnaInternacional@gmail.es");
+        imprimir(alumnoInter);
+        System.out.println("=======================¨Creando la instalan de progeso");
+        Profesor profesor = new Profesor("robertPRofe","PErez", "mates");
+        profesor.setEdad(45);
+        System.out.println("==================");
+        imprimir(profesor);
+        System.out.println("==================");
+
+
+
+    }
+    public static void imprimir(Persona persona){
+        System.out.println(persona.getNombre()+ " " + persona.getApellido() +  " " +persona.getEdad() + " " + persona.getEmail());
+        if (persona instanceof Alumno){
+            System.out.println("**************imprimeinto de los dato del tipo alumno");
+            System.out.println("Institución: "+ ((Alumno) persona).getInstituto());
+            System.out.println("Notas Matematica" + ((Alumno) persona).getNotasMates());
+            System.out.println("notas Historia" + ((Alumno) persona).getNotasHistoria());
+            System.out.println("notas Castellano"+ ((Alumno) persona).getNotasLengua());
+            System.out.println("Imprimiento los datos del itpo alumno internacion: ");
+            if (persona instanceof AlumnoInternacional){
+                System.out.println("notas idiiomas" + ((AlumnoInternacional) persona).getNotaIdiomas());
+                System.out.println("pais de alumno internacionl"+ ((AlumnoInternacional) persona).getPais());
+            }
+        }
+        if (persona instanceof Profesor){
+            System.out.println("Asignatura: " +((Profesor) persona).getAsignatura());
+        }
+    }
+}
